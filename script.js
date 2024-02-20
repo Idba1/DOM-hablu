73150fc032ef4ca1a24f3fa81d97720e142c4e68
Idba1 <moniraislamself@gmail.com>
2024-02-20 16:40:48 +0600

    name

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ id1.innerText = 'hello';
 
 let class1= document.getElementsByClassName("class1")[5];
 class1.innerHTML = 'hi';
+
+let heading = document.getElementsByName('heading')[0];
+heading.innerHTML = 'hey';
