794ffc9f1246ea4416b73b1296ea20fee77c19fc
Idba1 <moniraislamself@gmail.com>
2024-02-20 16:05:17 +0600

    id

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-document.getElementById('Id1')
+let id1 = document.getElementById('Id1');
+id1.innerText = 'hello';
+
+/* let class2 = document.getElementsByClassName("class1")[2];
+class2.innerHTML = 'hi'; */
