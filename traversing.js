6b4844500ef7bbed98357ae28e301cd45851ea6d
Idba1 <moniraislamself@gmail.com>
2024-02-20 19:35:11 +0600

    traversing

---
+++ traversing.js
@@ -0,0 +1,7 @@
+// console.log('hello from traversing');
+
+let pNote = document.querySelector('pTag');
+console.log(pTag.parentNode);
+
+let divNote = document.getElementById('main');
+console.log(divNote.childNodes);
