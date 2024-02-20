4a5ac7106a08a9e6ef7d37c003fbf92b748489d2
Idba1 <moniraislamself@gmail.com>
2024-02-20 18:17:55 +0600

    onclick function

--- event.js
+++ event.js
@@ -1,1 +1,10 @@
 console.log('hello');
+
+function clickMe(){
+    // console.log('hello everyone');
+    document.getElementById('id1').innerText = Date();
+}
+
+function btnClick(){
+    document.getElementById('heading2').innerText = "Congratulations";
+}
