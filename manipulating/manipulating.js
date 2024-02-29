6ab17efa5c770440fe5311c2fe06a30e81099d03
Idba1 <moniraislamself@gmail.com>
2024-02-29 20:59:14 +0600

    menu create

--- manipulating/manipulating.js
+++ manipulating/manipulating.js
@@ -15,3 +15,16 @@ document.body.appendChild(div);
 let script = document.createElement('script');
 script.src = 'idba.js';
 document.body.appendChild(script)
+
+
+// menu
+let menu = document.getElementById("menu");
+
+function createLi(para){
+    let li = document.createElement('li');
+    li.innerText = para;
+    // console.log(li);
+    return li;
+}
+menu.appendChild(createLi('Idba'))
+menu.appendChild(createLi('Islam'))
