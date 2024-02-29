fa4d22c218ec04b2d84b8929ae2bb97aa1a19163
Idba1 <moniraislamself@gmail.com>
2024-02-29 20:17:19 +0600

    traversing with child,node,children

--- traversing/traversig.js
+++ traversing/traversig.js
@@ -1,4 +1,9 @@
 console.log("hello");
 
 let traversing = document.getElementsByClassName('FirstChild')[0];
-console.log(traversing.parentElement.parentElement.parentElement)
+console.log(traversing.parentElement.parentElement.parentElement);
+
+console.log(traversing.parentElement.firstElementChild);
+console.log(traversing.parentElement.lastElementChild);
+console.log(traversing.parentElement.childNodes);
+console.log(traversing.parentElement.children);
