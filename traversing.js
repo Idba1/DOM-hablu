4b490040512533325df30dd4dc7a7a5231322d84
Idba1 <moniraislamself@gmail.com>
2024-02-29 19:32:53 +0600

    selector folder

--- traversing.js
+++
@@ -1,7 +0,0 @@
-// console.log('hello from traversing');
-
-let pNote = document.querySelector('pTag');
-console.log(pTag.parentNode);
-
-let divNote = document.getElementById('main');
-console.log(divNote.childNodes);
