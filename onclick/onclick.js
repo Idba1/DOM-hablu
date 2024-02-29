9513398fcb9ac12d54767e4d564bcb67f2df82bf
Idba1 <moniraislamself@gmail.com>
2024-02-29 20:00:59 +0600

    onclick with Date

--- onclick/onclick.js
+++ onclick/onclick.js
@@ -4,3 +4,8 @@ function clickMe(){
     let click = document.getElementById("clickMe");
     click.textContent = "text chang by js DOM"
 }
+
+function date(){
+    let date = document.getElementById('date');
+    date.innerText = Date()
+}
