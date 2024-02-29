9f186a8dfefb2df508fbc671ebfddcfdd4a33019
Idba1 <moniraislamself@gmail.com>
2024-02-29 20:27:56 +0600

    sibling

---
+++ traversing/sibling.js
@@ -0,0 +1,7 @@
+console.log("hello");
+
+
+let current = document.getElementsByClassName('current')[0];
+console.log(current.parentElement);
+console.log(current.previousElementSibling);
+console.log(current.nextElementSibling);
