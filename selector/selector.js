6ef0d5901226c5779940f228fb65b504b29e4a15
Idba1 <moniraislamself@gmail.com>
2024-02-29 19:39:55 +0600

    class selector with inner html

--- selector/selector.js
+++ selector/selector.js
@@ -3,3 +3,12 @@ console.log("hello");
 let id1 = document.getElementById('id1');
 id1.innerText = "text change";
 console.log(id1);
+
+
+let class1 = document.getElementsByClassName('class1')[0];
+class1.innerHTML = `
+<h1>Hello World </h1>
+<p> hi i'm Idba Islam. currently i'm learning jsDOM </p>
+
+`
+console.log(class1);
